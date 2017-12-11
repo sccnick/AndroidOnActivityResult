f532d72d77ebbd7bcf269b490ed5dce497b9ee48
Nick <sccnick@hotmail.com>
2017-12-12 05:05:28 +0700

    Complete

--- app/src/main/java/com/example/navadroid/androidonactivityresult/AnotherActivity.java
+++ app/src/main/java/com/example/navadroid/androidonactivityresult/AnotherActivity.java
@@ -3,23 +3,50 @@ package com.example.navadroid.androidonactivityresult;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 public class AnotherActivity extends AppCompatActivity {
 
     private static final int RESULT_OK = 0;
+    //    private final int REQUEST_CODE = 555;
+    private TextView tvFullName, tvSurName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_another);
+
+        tvFullName = (TextView) findViewById(R.id.tvFullname);
+        tvSurName = (TextView) findViewById(R.id.tvSurname);
+
+//        Bundle extras = getIntent().getExtras();
+
+        Intent intent = getIntent();
+
+        if (intent.hasExtra("Value1")) {
+            tvFullName.setText(intent.getExtras().getString("Value1"));
+        }
+        if(intent.hasExtra("Value2")) {
+            tvSurName.setText(intent.getExtras().getString("Value2"));
+        }
+
+
     }
 
     @Override
     public void finish() {
         Intent intent = new Intent();
-        intent.putExtra("returnKey1", "This is an apple.");
-        intent.putExtra("returnKey2", "This is a pen.");
-        setResult(RESULT_OK, intent); //
+
+//        if (((RadioButton) findViewById(R.id.rb_apple)).isChecked()) {
+//            intent.putExtra("returnKey1", "This is an apple.");
+//        }
+//        if (((RadioButton) findViewById(R.id.rb_pen)).isChecked()) {
+//            intent.putExtra("returnKey2", "This is a pen.");
+//        }
+
+        setResult(RESULT_OK, intent);
+        //
         super.finish();
     }
 }
--- app/src/main/java/com/example/navadroid/androidonactivityresult/MainActivity.java
+++ app/src/main/java/com/example/navadroid/androidonactivityresult/MainActivity.java
@@ -5,28 +5,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private final int REQUEST_CODE = 555;
+    private final int RESULT_OK = 0;
+    //public static final
 
     private Button button;
+
+    private EditText etFullName;
+    private EditText etSurname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        etFullName = (EditText)findViewById(R.id.et_full_name);
+        etSurname = (EditText)findViewById(R.id.et_surname);
+
         button = (Button) findViewById(R.id.button);
+
+
         button.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(getApplicationContext(), AnotherActivity.class);
-                i.putExtra("Value1", "This value one for AnotherActivity");
-                i.putExtra("Value2", "This value two for AnotherActivity");
-                // set the request code to any code you like,
-                // you can identify the callback via this code
+//                 putExtra to AnotherActivity
+//
+                i.putExtra("Value1", etFullName.getText().toString());
+                i.putExtra("Value2", etSurname.getText().toString());
+
+
+                setResult(RESULT_OK,i);
+                // set the request code to any code you like, you can identify the callback via this code
                 startActivityForResult(i, REQUEST_CODE);
             }
         });
@@ -38,7 +54,11 @@ public class MainActivity extends AppCompatActivity {
             if (data.hasExtra("returnKey1")) {
                 Toast.makeText(this, data.getExtras().getString("returnKey1"),
                         Toast.LENGTH_SHORT).show();
+            } else if (data.hasExtra("returnKey2")) {
+                Toast.makeText(this, data.getExtras().getString("returnKey2"),
+                        Toast.LENGTH_SHORT).show();
             }
+
         }
     }
 }
